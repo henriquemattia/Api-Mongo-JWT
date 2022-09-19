e47089a8bbc5cc23e13a22c8f392f891e8fc081f
henriquemattia <mattia.henrique@gmail.com>
2022-09-19 20:08:22 -0300

    Adicionando Bcrypt para a segurança de senhas

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,13 +1,25 @@
 import express from 'express'
-// import UserModel from '../models/User.js'
+import User from '../models/User.js'
 
 const router = express.Router()
 
-router.post("/register", (req, res)=>{
-    console.log(req.body);
+router.post("/register", async(req, res)=>{
+
+    const { email } = req.body
+
+    if(await User.findOne({ email })){
+        return res.status(400).json({
+            error: true,
+            message: "Email já cadastrado"
+        })
+    }
+
+    const Users = await User.create(req.body)
+
     return res.json({
         error: false,
-        message: "Register with success!"
+        message: "Register with success!",
+        data: Users
     })
 })
 
